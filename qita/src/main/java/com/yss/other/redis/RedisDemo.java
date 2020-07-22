da6d02ff26c51b83ffcf9da3755ef368a6c82bdf
duhao <duhao@ysstech.com>
2020-07-22 17:54:10 +0800

    Jedis 连接与测试

--- qita/src/main/java/com/yss/other/redis/RedisDemo.java
+++ qita/src/main/java/com/yss/other/redis/RedisDemo.java
@@ -1,11 +1,19 @@
 package com.yss.other.redis;
 
+
+import redis.clients.jedis.Jedis;
+
 /**redis 测试
  * @author: duhao
  * @date: 2020/7/9 9:08
  */
 
 public class RedisDemo {
+    public static void main(String[] args) {
+        Jedis jedis = new Jedis("192.168.232.111",6379);
+        // 测试连接成功
+        System.out.println(jedis.ping());
 
 
+    }
 }
