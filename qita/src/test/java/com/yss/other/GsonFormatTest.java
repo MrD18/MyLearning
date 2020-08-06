82a47c3f30b461255dde92c326ef1b189f9d8ca7
duhao <duhao@ysstech.com>
2020-08-06 10:01:13 +0800

    测试GsonFormat 根据json逆向生成实体类工具

---
+++ qita/src/test/java/com/yss/other/GsonFormatTest.java
@@ -0,0 +1,392 @@
+package com.yss.other;
+
+/**
+ * @author: duhao
+ * @date: 2020/8/6 9:12
+ */
+public class GsonFormatTest {
+
+    /**快捷键 Alt+S
+     * approveStatus : integer(int32)
+     * periodNum : integer(int32)
+     * sourceId : integer(int32)
+     * applyChangeId : integer(int32)
+     * exampleId : integer(int32)
+     * proBaseInfo : object
+     * submitType : integer(int32)
+     * mainBody : object
+     * interestRule : object
+     * stakeholder : object
+     * productName : string
+     * elementViewAuth : boolean
+     * workFlow : boolean
+     * instanceId : string
+     * workflowSelect : string
+     * draft : boolean
+     * editType : integer(int32)
+     * creditRating : object
+     * repayment : object
+     * elementSettings : array
+     * interestRate : object
+     * periodId : integer(int32)
+     * productId : integer(int32)
+     * editable : boolean
+     * addCredit : object
+     * count : integer(int32)
+     * productGradeInfo : object
+     * targetProject : object
+     * elementVOS : array
+     * userId : string
+     * productTypeId : integer(int32)
+     * elementSettingIds : string
+     * workflowCode : integer(int32)
+     * expenseScheme : object
+     * operationType : string
+     * changeLogId : integer(int32)
+     * sourceName : string
+     * paymentInfo : object
+     */
+
+    private String approveStatus;
+    private String periodNum;
+    private String sourceId;
+    private String applyChangeId;
+    private String exampleId;
+    private String proBaseInfo;
+    private String submitType;
+    private String mainBody;
+    private String interestRule;
+    private String stakeholder;
+    private String productName;
+    private String elementViewAuth;
+    private String workFlow;
+    private String instanceId;
+    private String workflowSelect;
+    private String draft;
+    private String editType;
+    private String creditRating;
+    private String repayment;
+    private String elementSettings;
+    private String interestRate;
+    private String periodId;
+    private String productId;
+    private String editable;
+    private String addCredit;
+    private String count;
+    private String productGradeInfo;
+    private String targetProject;
+    private String elementVOS;
+    private String userId;
+    private String productTypeId;
+    private String elementSettingIds;
+    private String workflowCode;
+    private String expenseScheme;
+    private String operationType;
+    private String changeLogId;
+    private String sourceName;
+    private String paymentInfo;
+
+    public String getApproveStatus() {
+        return approveStatus;
+    }
+
+    public void setApproveStatus(String approveStatus) {
+        this.approveStatus = approveStatus;
+    }
+
+    public String getPeriodNum() {
+        return periodNum;
+    }
+
+    public void setPeriodNum(String periodNum) {
+        this.periodNum = periodNum;
+    }
+
+    public String getSourceId() {
+        return sourceId;
+    }
+
+    public void setSourceId(String sourceId) {
+        this.sourceId = sourceId;
+    }
+
+    public String getApplyChangeId() {
+        return applyChangeId;
+    }
+
+    public void setApplyChangeId(String applyChangeId) {
+        this.applyChangeId = applyChangeId;
+    }
+
+    public String getExampleId() {
+        return exampleId;
+    }
+
+    public void setExampleId(String exampleId) {
+        this.exampleId = exampleId;
+    }
+
+    public String getProBaseInfo() {
+        return proBaseInfo;
+    }
+
+    public void setProBaseInfo(String proBaseInfo) {
+        this.proBaseInfo = proBaseInfo;
+    }
+
+    public String getSubmitType() {
+        return submitType;
+    }
+
+    public void setSubmitType(String submitType) {
+        this.submitType = submitType;
+    }
+
+    public String getMainBody() {
+        return mainBody;
+    }
+
+    public void setMainBody(String mainBody) {
+        this.mainBody = mainBody;
+    }
+
+    public String getInterestRule() {
+        return interestRule;
+    }
+
+    public void setInterestRule(String interestRule) {
+        this.interestRule = interestRule;
+    }
+
+    public String getStakeholder() {
+        return stakeholder;
+    }
+
+    public void setStakeholder(String stakeholder) {
+        this.stakeholder = stakeholder;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getElementViewAuth() {
+        return elementViewAuth;
+    }
+
+    public void setElementViewAuth(String elementViewAuth) {
+        this.elementViewAuth = elementViewAuth;
+    }
+
+    public String getWorkFlow() {
+        return workFlow;
+    }
+
+    public void setWorkFlow(String workFlow) {
+        this.workFlow = workFlow;
+    }
+
+    public String getInstanceId() {
+        return instanceId;
+    }
+
+    public void setInstanceId(String instanceId) {
+        this.instanceId = instanceId;
+    }
+
+    public String getWorkflowSelect() {
+        return workflowSelect;
+    }
+
+    public void setWorkflowSelect(String workflowSelect) {
+        this.workflowSelect = workflowSelect;
+    }
+
+    public String getDraft() {
+        return draft;
+    }
+
+    public void setDraft(String draft) {
+        this.draft = draft;
+    }
+
+    public String getEditType() {
+        return editType;
+    }
+
+    public void setEditType(String editType) {
+        this.editType = editType;
+    }
+
+    public String getCreditRating() {
+        return creditRating;
+    }
+
+    public void setCreditRating(String creditRating) {
+        this.creditRating = creditRating;
+    }
+
+    public String getRepayment() {
+        return repayment;
+    }
+
+    public void setRepayment(String repayment) {
+        this.repayment = repayment;
+    }
+
+    public String getElementSettings() {
+        return elementSettings;
+    }
+
+    public void setElementSettings(String elementSettings) {
+        this.elementSettings = elementSettings;
+    }
+
+    public String getInterestRate() {
+        return interestRate;
+    }
+
+    public void setInterestRate(String interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    public String getPeriodId() {
+        return periodId;
+    }
+
+    public void setPeriodId(String periodId) {
+        this.periodId = periodId;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getEditable() {
+        return editable;
+    }
+
+    public void setEditable(String editable) {
+        this.editable = editable;
+    }
+
+    public String getAddCredit() {
+        return addCredit;
+    }
+
+    public void setAddCredit(String addCredit) {
+        this.addCredit = addCredit;
+    }
+
+    public String getCount() {
+        return count;
+    }
+
+    public void setCount(String count) {
+        this.count = count;
+    }
+
+    public String getProductGradeInfo() {
+        return productGradeInfo;
+    }
+
+    public void setProductGradeInfo(String productGradeInfo) {
+        this.productGradeInfo = productGradeInfo;
+    }
+
+    public String getTargetProject() {
+        return targetProject;
+    }
+
+    public void setTargetProject(String targetProject) {
+        this.targetProject = targetProject;
+    }
+
+    public String getElementVOS() {
+        return elementVOS;
+    }
+
+    public void setElementVOS(String elementVOS) {
+        this.elementVOS = elementVOS;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getProductTypeId() {
+        return productTypeId;
+    }
+
+    public void setProductTypeId(String productTypeId) {
+        this.productTypeId = productTypeId;
+    }
+
+    public String getElementSettingIds() {
+        return elementSettingIds;
+    }
+
+    public void setElementSettingIds(String elementSettingIds) {
+        this.elementSettingIds = elementSettingIds;
+    }
+
+    public String getWorkflowCode() {
+        return workflowCode;
+    }
+
+    public void setWorkflowCode(String workflowCode) {
+        this.workflowCode = workflowCode;
+    }
+
+    public String getExpenseScheme() {
+        return expenseScheme;
+    }
+
+    public void setExpenseScheme(String expenseScheme) {
+        this.expenseScheme = expenseScheme;
+    }
+
+    public String getOperationType() {
+        return operationType;
+    }
+
+    public void setOperationType(String operationType) {
+        this.operationType = operationType;
+    }
+
+    public String getChangeLogId() {
+        return changeLogId;
+    }
+
+    public void setChangeLogId(String changeLogId) {
+        this.changeLogId = changeLogId;
+    }
+
+    public String getSourceName() {
+        return sourceName;
+    }
+
+    public void setSourceName(String sourceName) {
+        this.sourceName = sourceName;
+    }
+
+    public String getPaymentInfo() {
+        return paymentInfo;
+    }
+
+    public void setPaymentInfo(String paymentInfo) {
+        this.paymentInfo = paymentInfo;
+    }
+}
