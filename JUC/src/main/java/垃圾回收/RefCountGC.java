663794efd4e1787c5416e1428b23437270dc1f22
duhao <duhao@ysstech.com>
2020-08-20 16:13:36 +0800

    垃圾回收

--- JUC/src/main/java/垃圾回收/RefCountGC.java
+++ JUC/src/main/java/垃圾回收/RefCountGC.java
@@ -6,9 +6,10 @@ package 垃圾回收;
  */
 public class RefCountGC {
     // 这个成员属性的唯一作用就是占用一点内存
-   // private byte[] bigSize = new byte[5*1024*1024];
+    private byte[] bigSize = new byte[5 * 1024 * 1024];
     // 引用
     Object reference = null;
+
 
     public static void main(String[] args) {
         RefCountGC obj1 = new RefCountGC();
